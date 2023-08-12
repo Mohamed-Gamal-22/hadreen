789f3374344fa8b6b99c22a6ae11e4c95fee6dbf
mohamed gamal <mogamall086@gmail.com>
2023-08-12 18:22:56 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Bridge from './Components/Bridge/Bridge';
+import Footer from './Components/Footer/Footer';
+import Hadreen from './Components/Hadreen/Hadreen';
+import Header from './Components/Header/Header'; 
+import Popup from './Components/Popup/Popup';
+import Serve from './Components/serve/Serv';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Popup />
+      <Hadreen />
+      <Bridge />
+      <Serve />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
